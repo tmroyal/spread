bf140b5f4d0ae8fc52724ac191ceb599b54f376c
thomas royal <thomas.m.royal@gmail.com>
2023-01-29 21:32:54 -0500

    add: validation and clearing

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-document.body.onload(main());
+main();
 
 /*
   This program is a simple example of how to download a CSV file from a web page and how to create a CSV file from an array of data.
@@ -14,15 +14,58 @@ function main(){
       alert('Please fill in all fields');
       return;
     }
+    if (isNaN(age)){
+      alert('Age must be a number');
+      return;
+    }
     const data = [name, age];
-    document.querySelector('#data').innerHTML += `<tr><td>${data[0]}</td><td>${data[1]}</td></tr>`;
     csvData.push(data);
+    redrawTable(csvData);
+  });
+  
+  document.querySelector('#clear').addEventListener('click', () => {
+    const name = document.querySelector('#name');
+    const age = document.querySelector('#age');
+    name.value = '';
+    age.value = '';
   });
   
   document.querySelector('#save').addEventListener('click', () => {
     downloadCsv(exportToCsv(csvData));
   });
-  
+}
+
+function removeRow(data, index){
+  data.splice(index, 1);
+  redrawTable(data);
+}
+
+function redrawTable(data){
+  const table = document.querySelector('#data');
+  table.innerHTML = '';
+  data.forEach((row, i) => {
+    const tr = document.createElement('tr');
+    row.forEach((cell) => {
+      if (i === 0){
+        const th = document.createElement('th');
+        th.innerText = cell; 
+        tr.appendChild(th);
+      } else { 
+        const td = document.createElement('td');
+        td.innerText = cell;
+        tr.appendChild(td);
+      }
+    });
+    if (i !== 0) {
+      const removeButton = document.createElement('button');
+      removeButton.innerText = 'Remove';
+      removeButton.addEventListener('click', () => {
+        removeRow(data, data.indexOf(row));
+      });
+      tr.appendChild(removeButton);
+    }
+    table.appendChild(tr);
+  });
 }
 
 function exportToCsv(data){
